dc38aefb9a99f3f9ce4ff3362fdc6c3ceff14099
anowarulkarim <anowarulkarim8@gmail.com>
2023-11-18 00:43:16 +0600

    added router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import React from 'react';
 import './App.css';
 
 import Data from './data/data';
+import Home from './home/home';
+import { Link, Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <Data/>
+    <div class='div1'>
+    <nav>
+        <ul>
+          <li>
+            <Link to="/data">Data</Link>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+        </ul>
+      
+    </nav>
+    <Routes>
+      <Route path='/data' element={<Data/>}></Route>
+      <Route path='/' element={<Home/>}></Route>
+    </Routes>
     </div>
   );
 }
---
+++ src/home/home.js
@@ -0,0 +1,7 @@
+import React from "react";
+const home=()=>{
+    return(
+        <p>this is the home page</p>
+    )
+}
+export default home;
